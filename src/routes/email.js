020cc5d9198c22e9507bdec203f2b18d9a50b9ff
Kammradt <vinicius.kammradt1@gmail.com>
2020-02-05 21:29:32 -0300

    :sparkles: Adding email history

--- src/routes/email.js
+++ src/routes/email.js
@@ -1,24 +1,27 @@
 const express = require('express')
 const sgMail = require('@sendgrid/mail');
+const Email = require('./../db/models/email')
 
 sgMail.setApiKey(process.env.SENDGRID_TOKEN);
 
 const router = express.Router()
 
 router.post('/mail', async (request, response) => {
-  let emailInformation = request.body
+  let { email, name, score } = request.body
   const msg = {
     to: emailInformation.to,
     from: 'your-email@gmail.com',
     templateId: 'd-aaaabbbbcccddddeeee',
     dynamic_template_data: {
-      receiverName: emailInformation.receiverName,
-      resultValue: emailInformation.resultValue
+      receiverName: name,
+      resultValue: score
     },
   };
   try {
+    let emailHistory = new Email({ email, name, score })
+    await emailHistory.save()
     await sgMail.send(msg);
-    return response.send({ message: 'Email send!' })
+    return response.send({ message: 'Email sent!' })
   } catch (error) {
     return response.send({ error: `An error occurred: ${error}` })
   }
